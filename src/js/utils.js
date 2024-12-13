cdccd2c110a379888a03bbbe7ba7599a824e7bc5
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-12-13 04:48:46 -0600

    Random user info set to populate divs

--- src/js/utils.js
+++ src/js/utils.js
@@ -2,6 +2,10 @@ export function select(selector, scope = document) {
   return scope.querySelector(selector);
 }
 
+export function selectAll(selector, scope = document) {
+  return scope.querySelectorAll(selector);
+}
+
 export function listen(event, selector, callback) {
   return selector.addEventListener(event, callback);
 }
